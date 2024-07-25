146e7e68ccfe7e7686d80431e3240bc39e5e51ec
Alcides Neto <108681708+alcidesgoncalves@users.noreply.github.com>
2024-07-25 10:17:42 -0300

    feat: renamed variable names

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const btnCalc = document.getElementById("btn-calc");
-const budgetValue = document.getElementById("budget-value");
-const budgetMetric = document.getElementById("budget-metric");
+const budgetBtn = document.getElementById("btn-calc");
+const expenseBtn = document.getElementById("expense-btn");
+const budgetValue = document.getElementById("budget-metric");
+const budgetInput = document.getElementById("budget-value");
+const descInput = document.getElementById("expense-desc");
+const amountInput = document.getElementById("expense-amount");
 
-btnCalc.addEventListener("click", function () {
-  budgetMetric.innerText = `${"$ " + budgetValue.value}`;
+budgetBtn.addEventListener("click", function () {
+  budgetValue.innerText = `${"$ " + budgetInput.value}`;
+});
+
+expenseBtn.addEventListener("click", function () {
+  const expenseList = document.getElementById("expense-list");
+  const expenseItem = document.createElement("li");
+  expenseItem.className = "item";
+
+  expenseItem.innerHTML = `
+    <span>Desc: </span> ${descInput.value} 
+    <span>Amount: </span> ${"$" + amountInput.value} 
+  `;
+
+  expenseList.appendChild(expenseItem);
 });
